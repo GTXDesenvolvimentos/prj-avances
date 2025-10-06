0d4bbcf8ee7ee4f4426a3ed5bcbf78a6dcb6903f
GTXDesenvolvimento <gtx.desenvolvimento@gmail.com>
2025-10-06 19:23:14 -0300

    C!!orreção de deleção lóogi

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Psr7\Query;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\QueryException;
@@ -16,13 +17,27 @@ class ProductController extends Controller
         $limit = (int) $request->query('limit', 25);
         $search = trim($request->query('search', ''), '"\'');
 
+
         $query = ProductModel::with(['category', 'unit'])->where('company_id', $user->company_id);
+
+        $query = ProductModel::withTrashed()
+            ->with([
+                'category' => function ($q) {
+                    $q->withTrashed(); // inclui categorias soft deleted
+                },
+                'unit' => function ($q) {
+                    $q->withTrashed(); // inclui unidades soft deleted
+                }
+            ])
+            ->where('company_id', $user->company_id);
+
 
         if ($search) {
             $query->where(function ($q) use ($search) {
                 $q->where('product_code', 'LIKE', "%{$search}%")
                     ->orWhere('name', 'LIKE', "%{$search}%")
-                    ->orWhere('description', 'LIKE', "%{$search}%");
+                    ->orWhere('description', 'LIKE', "%{$search}%")
+                    ->withTrashed();
             });
         }
 
@@ -65,6 +80,7 @@ class ProductController extends Controller
                 'page' => $products->currentPage(),
                 'limit' => $products->perPage(),
                 'page_count' => $products->lastPage(),
+
                 'total_count' => $products->total(),
             ],
         ], 200);
@@ -80,7 +96,8 @@ class ProductController extends Controller
         $product = ProductModel::with(['category', 'unit'])
             ->where('id', $id)
             ->where('company_id', $user->company_id)
-            ->first();
+            ->first()
+            ->withTrashed();
 
         if (!$product) {
             return response()->json([
