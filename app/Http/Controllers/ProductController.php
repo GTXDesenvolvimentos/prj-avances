d40bf5cb1ac5612cbfd20f42ddec85b57b82a979
GTXDesenvolvimento <gtx.desenvolvimento@gmail.com>
2025-10-06 17:17:42 -0300

    C!!orreção de deleção lóogica

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -67,6 +67,7 @@ class ProductController extends Controller
                 'page' => $products->currentPage(),
                 'limit' => $products->perPage(),
                 'page_count' => $products->lastPage(),
+                
                 'total_count' => $products->total(),
             ],
         ], 200);
