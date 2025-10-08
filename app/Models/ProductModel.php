bd62a4943d2e8e58dfae56c3b9f6e0615edc1d0e
GTXDesenvolvimentos <128343664+GTXDesenvolvimentos@users.noreply.github.com>
2025-10-08 15:44:39 -0300

    Correção de exibição de produtos deletados

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -13,11 +13,11 @@ class ProductModel extends Model
     protected $table = 'products'; // nome da tabela
     protected $fillable = [
         'id',
-        'unit_id',
         'category_id',
+        'unit_id',
         'company_id',
         'product_code',
-        "name",
+        'name',
         'description',
         'availability',
         'average_cost',
@@ -25,66 +25,20 @@ class ProductModel extends Model
         'rental_price',
         'is_dynamic_sale_price',
         'is_dynamic_rental_price',
+        'status',
+        'created_at',
+        'updated_at',
+        'deleted_at'
     ];
     protected $casts = [
         'is_dynamic_sale_price' => 'boolean',
         'is_dynamic_rental_price' => 'boolean',
     ];
 
-    public $timestamps = false; // sua tabela não tem created_at/updated_at
 
-     // Relacionamento com Category
-    public function category()
-    {
-        return $this->belongsTo(ProductCategoryModel::class);
-    }
 
-    // Relacionamento com Unit
-    public function unit()
-    {
-        return $this->belongsTo(ProductUnitsModel::class);
-    }
 
-    // ============================
-    // Métodos CRUD personalizados
-    // ============================
-    public static function insertProduct(array $data)
-    {
-        return self::create($data);
-    }
 
-    // Atualizar produto por ID
-    public static function updateProduct(int $id, array $data)
-    {
-        $product = self::find($id);
-        if ($product) {
-            $product->update($data);
-            return $product;
-        }
-        return null; // produto não encontrado
-    }
-
-    // Deletar produto por ID
-    public static function deleteProduct(int $id)
-    {
-        $product = self::find($id);
-        if ($product) {
-            return $product->delete();
-        }
-        return false; // produto não encontrado
-    }
-
-    // Selecionar todos os produtos
-    public static function getAllProducts()
-    {
-        return self::all();
-    }
-
-    // Selecionar produto por ID
-    public static function getProductById(int $id)
-    {
-        return self::find($id);
-    }
 
     protected $dates = ['deleted_at'];
 }
