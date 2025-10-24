a96bfd0145c23cb8855537716ea0555961734cad
Rodrigo Batista <rodrigo.bsilva@somoseducacao.com.br>
2025-10-24 11:19:16 -0300

    fix(cors): add config

---
+++ config/cors.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://avances.vercel.app',
+        'http://localhost:3000', // pra testes locais
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
